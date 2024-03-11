bd3cf9717de65231b1bba0007ab1ec815107c25f
davidsebesta1 <david.sebesta@post.cz>
2024-03-11 17:25:09 +0100

    t

--- SniperLog/Platforms/Android/MainApplication.cs
+++ SniperLog/Platforms/Android/MainApplication.cs
@@ -1,5 +1,10 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
+using Android.Gms.Maps;
+using Android.Health.Connect.DataTypes;
 using Android.Runtime;
+using Java.Util.Jar;
+using Xamarin.Google.Crypto.Tink.Integration.Android;
 
 namespace SniperLog
 {
@@ -7,9 +12,9 @@ namespace SniperLog
     [Application]
     public class MainApplication : MauiApplication
     {
-        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-            : base(handle, ownership)
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership): base(handle, ownership)
         {
+           
         }
 
         protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
